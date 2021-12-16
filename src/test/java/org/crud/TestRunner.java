ac0561e581102d8c098bf3385f3f51c300ce7c54
Emily Chimbo <chimboemily@gmail.com>
2021-12-15 22:38:50 -0500

    fix:nombre persona

--- src/test/java/org/crud/TestRunner.java
+++ src/test/java/org/crud/TestRunner.java
@@ -5,7 +5,7 @@
 package org.crud;
 
 import org.crud.empleados.dao.GestionarDB;
-import org.crud.empleados.dao.GestionarPersona;
+import org.crud.empleados.dao.GestionarEmpleado;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
@@ -13,7 +13,7 @@ import org.junit.runner.notification.Failure;
 public class TestRunner {
     public static void main(String[] args) {
       Result result = JUnitCore.runClasses(GestionarDB.class);
-      Result result1=JUnitCore.runClasses(GestionarPersona.class);
+      Result result1=JUnitCore.runClasses(GestionarEmpleado.class);
 
       for (Failure failure : result.getFailures()) {
          System.out.println(failure.toString());
